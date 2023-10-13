53375f590d27f5738073265cd490ab43c52c6873
MaksimIvshin <mi375293086861@gmail.com>
2023-10-13 09:28:43 +0300

    Added alert about no internet connection.

--- PokemonApp/Networking/APICaller.swift
+++ PokemonApp/Networking/APICaller.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import UIKit
 
 // MARK: - Cases for possible error.
 enum NetworkError: Error {
@@ -13,32 +14,32 @@ enum NetworkError: Error {
     case canNotParseData
     case noInternetConnection
 }
-// MARK: - Class for making API request.
+// MARK: - Class for making API request and show alert.
 public class APICaller {
-    static func getPokemons(page: Int, completionHandler: @escaping (Result<PokemonPage, NetworkError>) -> Void) {
+    static func getPokemons(page: Int, compltitionHandler: @escaping (Result<PokemonPage, NetworkError>) -> Void) {
         var components = NetworkingURL.shared.serverAddress
         components.queryItems = [
             URLQueryItem(name: "limit", value: "10"),
             URLQueryItem(name: "offset", value: "\(page)")]
         guard let paginatedURL = components.url else {
-            completionHandler(.failure(.urlError))
+            compltitionHandler(.failure(.urlError))
             return
         }
         URLSession.shared.dataTask(with: paginatedURL) { dataResponse, urlResponse, error in
             if let error = error as NSError?, error.domain == NSURLErrorDomain && error.code == NSURLErrorNotConnectedToInternet {
-                completionHandler(.failure(.noInternetConnection))
+                compltitionHandler(.failure(.noInternetConnection))
+                showAlert()
                 return
             }
             if error == nil, let data = dataResponse {
                 if let resultData = try? JSONDecoder().decode(PokemonPage.self, from: data) {
-                    completionHandler(.success(resultData))
+                    compltitionHandler(.success(resultData))
                 } else {
-                    completionHandler(.failure(.canNotParseData))
+                    compltitionHandler(.failure(.canNotParseData))
                 }
             }
         }.resume()
     }
-
     static func getDetailPokemon(url: String, compltitionHandler: @escaping (_ result: Result<PokemonSelected, NetworkError>) -> Void) {
         let urlString = url
         guard let url = URL(string: urlString) else {
@@ -48,6 +49,7 @@ public class APICaller {
         URLSession.shared.dataTask(with: url) { dataResponse, urlResponse, error in
             if let error = error as NSError?, error.domain == NSURLErrorDomain && error.code == NSURLErrorNotConnectedToInternet {
                 compltitionHandler(.failure(.noInternetConnection))
+                showAlert()
                 return
             }
             if error == nil, let data = dataResponse {
@@ -58,5 +60,23 @@ public class APICaller {
                 }
             }
         }.resume()
+    }
+    static func showAlert() {
+        DispatchQueue.main.async {
+            guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+                  let rootViewController = windowScene.windows.first?.rootViewController else {
+                return
+            }
+            var topViewController = rootViewController
+            while let presentedViewController = topViewController.presentedViewController {
+                topViewController = presentedViewController
+            }
+            let alert = UIAlertController(title: "No Internet Connection",
+                                          message: "Please check your internet connection and try again.",
+                                          preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alert.addAction(okAction)
+            topViewController.present(alert, animated: true, completion: nil)
+        }
     }
 }
